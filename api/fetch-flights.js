4384e927dd8410be706a059472aaccba1d81faf5
Sergiu <sergiu.uhu@gmail.com>
2024-01-09 12:28:33 +0200

    test

---
+++ api/fetch-flights.js
@@ -0,0 +1,25 @@
+import { airports } from "./airports.js"
+import { searchFlights } from "./extra.js"
+
+export const config = {
+    runtime: 'edge',
+};
+
+export default async function handler(request) {
+    const urlParams = new URL(request.url).searchParams;
+    const query = Object.fromEntries(urlParams);
+
+    const airportCodes = airports.filter(o => o.country === query.country).map(o => o.code)
+
+    const flights = await searchFlights(airportCodes);
+
+    return new Response(
+        JSON.stringify(flights),
+        {
+            status: 200,
+            headers: {
+                'content-type': 'application/json',
+            },
+        },
+    );
+}
