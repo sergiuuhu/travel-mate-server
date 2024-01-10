229f12ec72554ddd67dbcec626b6b682f4c000d0
Sergiu <sergiu.uhu@gmail.com>
2024-01-10 23:59:40 +0200

    lets test

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import useLocalStorage from './utils/useLocalStorage';
 
-export default function Home() {
+export default function Home({ flights, ipInfo }) {
   const [isReady, setIsReady] = React.useState(false);
-  const [ipInfo, setIpInfo] = useLocalStorage('ipInfo', {});
 
   React.useEffect(() => {
     setTimeout(() => setIsReady(true), 1250);
   }, []);
 
-  React.useEffect(() => {
-    if (isReady && (!ipInfo || !Object.keys(ipInfo).length)) {
-      getIpInfo()
-        .then((ipInfo) => setIpInfo(ipInfo))
-        .catch((error) => console.error(error));
-    }
-  }, [isReady, ipInfo]);
-
   if (!isReady) {
     return <BrandedOverlay />
   }
 
+  console.log(flights, ipInfo)
+
   return <div>Home</div>;
+}
+
+export async function getServerSideProps() {
+  const flights = await fetchFlights()
+  const ipInfo = await getIpInfo()
+
+  return { props: { flights, ipInfo } }
 }
 
 const BrandedOverlay = () => {
@@ -38,11 +38,29 @@ const BrandedOverlay = () => {
 };
 
 const getIpInfo = () => new Promise((resolve, reject) => {
-  fetch(`https://ipinfo.io?token=${process.env.NEXT_PUBLIC_IP_INFO_TOKEN}`)
+  fetch(`https://ipinfo.io?token=${process.env.NEXT_PUBLIC_IP_INFO_TOKEN}`, { next: { revalidate: 604800 } })
     .then((response) => response.json())
     .then((data) => {
       if (!data.error) {
         resolve(data);
+      } else {
+        reject(new Error(data.error));
+      }
+    })
+    .catch((err) => reject(err));
+});
+
+const fetchFlights = () => new Promise((resolve, reject) => {
+  fetch(`http://localhost:3000//api/fetch-flights`, { next: { revalidate: 28800 } })
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.error) {
+        const newObj = {};
+        for (const flight of data) {
+          if (!newObj[flight['country_from']]) newObj[flight['country_from']] = []
+          newObj[flight['country_from']].push(flight)
+        }
+        resolve(newObj);
       } else {
         reject(new Error(data.error));
       }
