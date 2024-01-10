e0c06a42396779f45e0a9f8727a4227f06e0dac6
Sergiu <sergiu.uhu@gmail.com>
2024-01-10 14:45:51 +0200

    new changes

--- pages/api/_extra.js
+++ pages/api/_extra.js
@@ -9,266 +9,258 @@ const supabaseKey = process.env.SUPA_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const getFlights = async (params) => {
-  const url = "https://api.tequila.kiwi.com/v2/search";
-  const headers = {
-    accept: "application/json",
-    apikey: process.env.KIWI_API_KEY, // Assuming you use an environment variable for the API key
-  };
+    const url = "https://api.tequila.kiwi.com/v2/search";
+    const headers = {
+        accept: "application/json",
+        apikey: process.env.KIWI_API_KEY, // Assuming you use an environment variable for the API key
+    };
 
-  const queryParams = new URLSearchParams(params);
-  const fullUrl = `${url}?${queryParams.toString()}`;
+    const queryParams = new URLSearchParams(params);
+    const fullUrl = `${url}?${queryParams.toString()}`;
 
-  try {
-    const response = await fetch(fullUrl, { headers });
+    try {
+        const response = await fetch(fullUrl, { headers });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error("Error:", error.message);
+
+        throw error;
     }
-
-    const data = await response.json();
-    return data.data;
-  } catch (error) {
-    console.error("Error:", error.message);
-
-    throw error;
-  }
 };
 
 export const generateDates = (weeks) => {
-  const currentDate = new Date();
+    const weekStart = moment().add(weeks, 'weeks').startOf('week').toISOString();
 
-  currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 3); // Set to Thursday of the current week
+    const datesToArray = [
+        moment(weekStart).add(6, 'days').format('DD/MM/YYYY'),
+        moment(weekStart).add(7, 'days').format('DD/MM/YYYY'),
+        moment(weekStart).add(8, 'days').format('DD/MM/YYYY'),
+    ]
 
-  const daysToAdd = weeks * 7;
+    const daysFromArray = [3, 4, 5, 6]
 
-  const dateFromThursday = new Date(currentDate);
-  dateFromThursday.setDate(
-    currentDate.getDate() + ((3 - currentDate.getDay() + 7) % 7) + daysToAdd,
-  );
+    const dates = []
 
-  const dateFromFriday = new Date(dateFromThursday);
-  dateFromFriday.setDate(dateFromThursday.getDate() + 1);
+    for (const days of daysFromArray) {
+        for (const date_to of datesToArray) {
+            dates.push({
+                date_from: moment(weekStart).add(days, 'days').format("DD/MM/YYYY"),
+                date_to
+            })
+        }
+    }
 
-  const dateFromSaturday = new Date(dateFromThursday);
-  dateFromSaturday.setDate(dateFromThursday.getDate() + 2);
-
-  const dateToSunday = new Date(dateFromThursday);
-  dateToSunday.setDate(dateFromThursday.getDate() + 3);
-
-  const date_to = formatDate(dateToSunday);
-
-  const result = [
-    { date_from: formatDate(dateFromThursday), date_to },
-    { date_from: formatDate(dateFromFriday), date_to },
-    { date_from: formatDate(dateFromSaturday), date_to },
-  ];
-
-  return result;
+    return dates;
 };
 
 export const generateFlightOptions = (
-  flyFrom,
-  weeks = 1,
-  options = {
-    max_stopovers: 0,
-    sort: "price",
-    limit: 10, // Max is 1000
-    adults: 1,
-    max_fly_duration: 3,
-    price_from: 0,
-    price_to: 100,
-    max_stopovers: 0,
-    // 'max_sector_stopovers': 0,
-    curr: "EUR",
-    locale: "en",
-    ret_from_diff_city: false,
-    ret_to_diff_city: false,
-  },
+    flyFrom,
+    weeks = 1,
+    options = {
+        max_stopovers: 0,
+        sort: "price",
+        limit: 10, // Max is 1000
+        adults: 1,
+        max_fly_duration: 3,
+        price_from: 0,
+        price_to: 100,
+        max_stopovers: 0,
+        // 'max_sector_stopovers': 0,
+        curr: "EUR",
+        locale: "en",
+        ret_from_diff_city: false,
+        ret_to_diff_city: false,
+    },
 ) => {
-  // curr: AED, AFN, ALL, AMD, ANG, AOA, ARS, AUD, AWG, AZN, BAM, BBD, BDT, BGN, BHD, BIF, BMD, BND, BOB, BRL, BSD, BTC, BTN, BWP, BYN, BZD, CAD, CDF, CHF, CLF, CLP, CNY, COP, CRC, CUC, CUP, CVE, CZK, DJF, DKK, DOP, DZD, EEK, EGP, ERN, ETB, EUR, FJD, FKP, GBP, GEL, GGP, GHS, GIP, GMD, GNF, GTQ, GYD, HKD, HNL, HRK, HTG, HUF, IDR, ILS, IMP, INR, IQD, IRR, ISK, JEP, JMD, JOD, JPY, KES, KGS, KHR, KMF, KPW, KRW, KWD, KYD, KZT, LAK, LBP, LKR, LRD, LSL, LTL, LVL, LYD, MAD, MDL, MGA, MKD, MMK, MNT, MOP, MRO, MTL, MUR, MVR, MWK, MXN, MYR, MZN, NAD, NGN, NIO, NOK, NPR, NZD, OMR, PAB, PEN, PGK, PHP, PKR, PLN, PYG, QAR, QUN, RON, RSD, RUB, RWF, SAR, SBD, SCR, SDG, SEK, SGD, SHP, SLL, SOS, SRD, STD, SVC, SYP, SZL, THB, TJS, TMT, TND, TOP, TRY, TTD, TWD, TZS, UAH, UGX, USD, UYU, UZS, VEF, VND, VUV, WST, XAF, XCD, XOF, XPF, YER, ZAR, ZMK, ZMW, ZWL
-  // locale: ae, ag, ar, at, au, be, bg, bh, br, by, ca, ca-fr, ch, cl, cn, co, ct, cz, da, de, dk, ec, ee, el, en, es, fi, fr, gb, gr, hk, hr, hu, id, ie, il, in, is, it, ja, jo, jp, ko, kr, kw, kz, lt, mx, my, nl, no, nz, om, pe, ph, pl, pt, qa, ro, rs, ru, sa, se, sg, sk, sr, sv, th, tr, tw, ua, uk, us, vn, za
+    // curr: AED, AFN, ALL, AMD, ANG, AOA, ARS, AUD, AWG, AZN, BAM, BBD, BDT, BGN, BHD, BIF, BMD, BND, BOB, BRL, BSD, BTC, BTN, BWP, BYN, BZD, CAD, CDF, CHF, CLF, CLP, CNY, COP, CRC, CUC, CUP, CVE, CZK, DJF, DKK, DOP, DZD, EEK, EGP, ERN, ETB, EUR, FJD, FKP, GBP, GEL, GGP, GHS, GIP, GMD, GNF, GTQ, GYD, HKD, HNL, HRK, HTG, HUF, IDR, ILS, IMP, INR, IQD, IRR, ISK, JEP, JMD, JOD, JPY, KES, KGS, KHR, KMF, KPW, KRW, KWD, KYD, KZT, LAK, LBP, LKR, LRD, LSL, LTL, LVL, LYD, MAD, MDL, MGA, MKD, MMK, MNT, MOP, MRO, MTL, MUR, MVR, MWK, MXN, MYR, MZN, NAD, NGN, NIO, NOK, NPR, NZD, OMR, PAB, PEN, PGK, PHP, PKR, PLN, PYG, QAR, QUN, RON, RSD, RUB, RWF, SAR, SBD, SCR, SDG, SEK, SGD, SHP, SLL, SOS, SRD, STD, SVC, SYP, SZL, THB, TJS, TMT, TND, TOP, TRY, TTD, TWD, TZS, UAH, UGX, USD, UYU, UZS, VEF, VND, VUV, WST, XAF, XCD, XOF, XPF, YER, ZAR, ZMK, ZMW, ZWL
+    // locale: ae, ag, ar, at, au, be, bg, bh, br, by, ca, ca-fr, ch, cl, cn, co, ct, cz, da, de, dk, ec, ee, el, en, es, fi, fr, gb, gr, hk, hr, hu, id, ie, il, in, is, it, ja, jo, jp, ko, kr, kw, kz, lt, mx, my, nl, no, nz, om, pe, ph, pl, pt, qa, ro, rs, ru, sa, se, sg, sk, sr, sv, th, tr, tw, ua, uk, us, vn, za
 
-  const params = {
-    fly_from: flyFrom,
-    fly_to: "",
-    ...options,
-  };
-
-  const dates = generateDates(weeks);
-  const flightOptions = [];
-
-  for (const dateRange of dates) {
-    const flightOption = {
-      ...params,
-      date_from: dateRange.date_from,
-      date_to: dateRange.date_from,
-      return_from: dateRange.date_to,
-      return_to: dateRange.date_to,
+    const params = {
+        fly_from: flyFrom,
+        fly_to: "",
+        ...options,
     };
-    flightOptions.push(flightOption);
-  }
 
-  return flightOptions;
+    const dates = generateDates(weeks);
+    const flightOptions = [];
+
+    for (const dateRange of dates) {
+        const flightOption = {
+            ...params,
+            date_from: dateRange.date_from,
+            date_to: dateRange.date_from,
+            return_from: dateRange.date_to,
+            return_to: dateRange.date_to,
+        };
+        flightOptions.push(flightOption);
+    }
+
+    return flightOptions;
 };
 
 const formatFlightData = (flight) => {
-  const flight1 = flight["route"][0];
-  const flight2 = flight["route"][1];
+    const flight1 = flight["route"][0];
+    const flight2 = flight["route"][1];
 
-  const mainObject = {
-    // 'kiwi_id': flight['id'],
-    // 'combination_id': flight['combination_id'],
-    price_eur: flight["price"],
-    // 'flight1_country_from': flight1['countryFrom']['name'],
-    flight1_city_from: flight1["cityFrom"],
-    flight1_fly_from: flight1["flyFrom"],
-    // 'flight1_country_to': flight1['countryTo']['name'],
-    flight1_city_to: flight1["cityTo"],
-    flight1_fly_to: flight1["flyTo"],
-    flight1_departure_time: flight1["local_departure"],
-    flight1_arrival_time: flight1["local_arrival"],
-    flight1_duration: calculateDuration(
-      flight1["utc_departure"],
-      flight1["utc_arrival"],
-    ),
-    flight1_airline: flight1["airline"],
-    // 'flight2_country_from': flight2['countryFrom']['name'],
-    flight2_city_from: flight2["cityFrom"],
-    flight2_fly_from: flight2["flyFrom"],
-    // 'flight2_country_to': flight2['countryTo']['name'],
-    flight2_city_to: flight2["cityTo"],
-    flight2_fly_to: flight2["flyTo"],
-    flight2_departure_time: flight2["local_departure"],
-    flight2_arrival_time: flight2["local_arrival"],
-    flight2_duration: calculateDuration(
-      flight2["utc_departure"],
-      flight2["utc_arrival"],
-    ),
-    flight2_airline: flight2["airline"],
-    availability: flight["availability"]["seats"],
-    booking_link: flight["deep_link"],
-    // 'layovers': flight['route'].length - 1
-  };
+    const mainObject = {
+        // 'kiwi_id': flight['id'],
+        // 'combination_id': flight['combination_id'],
+        price_eur: flight["price"],
+        // 'flight1_country_from': flight1['countryFrom']['name'],
+        flight1_city_from: flight1["cityFrom"],
+        flight1_fly_from: flight1["flyFrom"],
+        // 'flight1_country_to': flight1['countryTo']['name'],
+        flight1_city_to: flight1["cityTo"],
+        flight1_fly_to: flight1["flyTo"],
+        flight1_departure_time: flight1["local_departure"],
+        flight1_arrival_time: flight1["local_arrival"],
+        flight1_duration: calculateDuration(
+            flight1["utc_departure"],
+            flight1["utc_arrival"],
+        ),
+        flight1_airline: flight1["airline"],
+        // 'flight2_country_from': flight2['countryFrom']['name'],
+        flight2_city_from: flight2["cityFrom"],
+        flight2_fly_from: flight2["flyFrom"],
+        // 'flight2_country_to': flight2['countryTo']['name'],
+        flight2_city_to: flight2["cityTo"],
+        flight2_fly_to: flight2["flyTo"],
+        flight2_departure_time: flight2["local_departure"],
+        flight2_arrival_time: flight2["local_arrival"],
+        flight2_duration: calculateDuration(
+            flight2["utc_departure"],
+            flight2["utc_arrival"],
+        ),
+        flight2_airline: flight2["airline"],
+        availability: flight["availability"]["seats"],
+        booking_link: flight["deep_link"],
+        // 'layovers': flight['route'].length - 1
+    };
 
-  return mainObject;
+    return mainObject;
 };
 
 export const getAirlineLogo = (airline) => {
-  return `https://images.kiwi.com/airlines/32x32/${airline}.png?default=airline.png`;
+    return `https://images.kiwi.com/airlines/32x32/${airline}.png?default=airline.png`;
 };
 
 export const formatFlightDataList = (flights) => {
-  const formattedData = flights.map((flight) => formatFlightData(flight));
-  return formattedData;
+    const formattedData = flights.map((flight) => formatFlightData(flight));
+    return formattedData;
 };
 
 export const formatDate = (date) => {
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
 };
 
 export const transformTimestamp = (dateString) => {
-  const timestamp = moment(dateString, "YYYY-MM-DDTHH:mm:ss.SSSZ");
-  const formattedTimestamp = timestamp.format("MMM DD, YYYY, d HH:mm");
-  return formattedTimestamp;
+    const timestamp = moment(dateString, "YYYY-MM-DDTHH:mm:ss.SSSZ");
+    const formattedTimestamp = timestamp.format("MMM DD, YYYY, d HH:mm");
+    return formattedTimestamp;
 };
 
 export const calculateDuration = (utcDeparture, utcArrival) => {
-  const departureTime = new Date(utcDeparture);
-  const arrivalTime = new Date(utcArrival);
+    const departureTime = new Date(utcDeparture);
+    const arrivalTime = new Date(utcArrival);
 
-  const durationMillis = arrivalTime - departureTime;
+    const durationMillis = arrivalTime - departureTime;
 
-  // Calculate hours and minutes
-  const hours = Math.floor(durationMillis / (1000 * 60 * 60));
-  const minutes = Math.floor((durationMillis % (1000 * 60 * 60)) / (1000 * 60));
+    // Calculate hours and minutes
+    const hours = Math.floor(durationMillis / (1000 * 60 * 60));
+    const minutes = Math.floor((durationMillis % (1000 * 60 * 60)) / (1000 * 60));
 
-  // Format the result
-  const formattedDuration = `${hours}h ${String(minutes).padStart(2, "0")}'`;
+    // Format the result
+    const formattedDuration = `${hours}h ${String(minutes).padStart(2, "0")}'`;
 
-  return formattedDuration;
+    return formattedDuration;
 };
 
 const delay = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
 export const searchFlights = async (airportCodes) => {
-  const { data, error } = await supabase
-    .from("flights")
-    .select()
-    .in("flight1_fly_from", airportCodes);
+    const { data, error } = await supabase
+        .from("flights")
+        .select()
+        .in("flight1_fly_from", airportCodes);
 
-  return data;
+    return data;
 };
 
 export const letsGo = async () => {
-  const airportIndexRows = await supabase
-    .from("variables")
-    .select("value")
-    .eq("key", "airportIndex");
-  const numberOfWeeksRows = await supabase
-    .from("variables")
-    .select("value")
-    .eq("key", "numberOfWeeks");
+    const airportIndexRows = await supabase
+        .from("variables")
+        .select("value")
+        .eq("key", "airportIndex");
+    const numberOfWeeksRows = await supabase
+        .from("variables")
+        .select("value")
+        .eq("key", "numberOfWeeks");
 
-  const airportIndex = parseInt(airportIndexRows.data[0].value);
-  const weeks = parseInt(numberOfWeeksRows.data[0].value);
+    const airportIndex = parseInt(airportIndexRows.data[0].value);
+    const weeks = parseInt(numberOfWeeksRows.data[0].value);
 
-  const flyFrom = airports[airportIndex]["code"];
+    const flyFrom = airports[airportIndex]["code"];
 
-  const flightOptions = generateFlightOptions(flyFrom, weeks);
+    const flightOptions = generateFlightOptions(flyFrom, weeks);
 
-  let allFlights = [];
+    let allFlights = [];
 
-  for (const option of flightOptions) {
-    const flights = await getFlights(option);
+    for (const option of flightOptions) {
+        const flights = await getFlights(option);
 
-    allFlights = [...allFlights, ...flights];
+        allFlights = [...allFlights, ...flights];
 
-    await delay(1000);
-  }
+        await delay(1000);
+    }
 
-  const formatted = allFlights
-    .filter((o) => o["route"] && o["route"].length === 2)
-    .map((o) => formatFlightData(o));
+    const formatted = allFlights
+        .filter((o) => o["route"] && o["route"].length === 2)
+        .map((o) => formatFlightData(o));
 
-  const dates = generateDates(weeks);
+    const dates = generateDates(weeks);
 
-  const dateTo = dates[0]["date_to"];
-  const dayStart = moment(dateTo, "DD/MM/YYYY")
-    .subtract(1, "day")
-    .toISOString();
-  const dayEnd = moment(dateTo, "DD/MM/YYYY").add(1, "day").toISOString();
+    const dayStart = moment(dates[0]["date_to"], "DD/MM/YYYY")
+        .subtract(1, "day")
+        .toISOString();
+    const dayEnd = moment(dates[2]["date_to"], "DD/MM/YYYY").add(1, "day").toISOString();
 
-  const deletion = await supabase
-    .from("flights")
-    .delete()
-    .eq("flight1_fly_from", flyFrom)
-    .gt("flight2_arrival_time", dayStart)
-    .lt("flight2_arrival_time", dayEnd);
+    const deletion = await supabase
+        .from("flights")
+        .delete()
+        .eq("flight1_fly_from", flyFrom)
+        .gt("flight2_arrival_time", dayStart)
+        .lt("flight2_arrival_time", dayEnd);
 
-  console.log("deletion", deletion, dayStart, dayEnd);
+    // console.log("deletion", deletion, dayStart, dayEnd);
 
-  const creation = await supabase.from("flights").insert(formatted);
+    const creation = await supabase.from("flights").insert(formatted);
 
-  // console.log("creation", creation)
+    // console.log("creation", creation)
 
-  const newNmberOfWeeks = weeks >= 12 ? 1 : weeks + 1;
-  await supabase
-    .from("variables")
-    .update({ value: newNmberOfWeeks, updated_at: moment().toISOString() })
-    .eq("key", "numberOfWeeks");
-
-  if (newNmberOfWeeks === 1) {
-    const newAirportIndex = airports[airportIndex + 1] ? airportIndex + 1 : 0;
+    const newNmberOfWeeks = weeks >= 12 ? 1 : weeks + 1;
     await supabase
-      .from("variables")
-      .update({ value: newAirportIndex, updated_at: moment().toISOString() })
-      .eq("key", "airportIndex");
-  }
+        .from("variables")
+        .update({ value: newNmberOfWeeks, updated_at: moment().toISOString() })
+        .eq("key", "numberOfWeeks");
 
-  return formatted.length;
+    if (newNmberOfWeeks === 1) {
+        const newAirportIndex = airports[airportIndex + 1] ? airportIndex + 1 : 0;
+        await supabase
+            .from("variables")
+            .update({ value: newAirportIndex, updated_at: moment().toISOString() })
+            .eq("key", "airportIndex");
+    }
+
+    return formatted.length;
 };
