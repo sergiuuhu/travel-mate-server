46482b135b948c0069f79a4d8c0a652574601df1
Sergiu <sergiu.uhu@gmail.com>
2024-01-10 23:33:40 +0200

    analytics added

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -43,6 +43,19 @@ export default function Layout({ children }) {
           content="Short city breaks for under $100. Arrive Saturday morning & back Sunday evening. Updated daily."
         />
         <meta property="twitter:image" content="https://www.citybreak.pro/cover.png" />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6CCP477D8C"></script>
+        <script>{`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-6CCP477D8C');`}
+        </script>
+        <script type="text/javascript">{`(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "kjuo8wbycs");`}</script>
       </Head>
 
       <main>{children}</main>
