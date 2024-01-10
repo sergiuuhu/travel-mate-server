1f221edb6f16ac1bb8c3c434fc0eeae4568684f4
Sergiu <sergiu.uhu@gmail.com>
2024-01-11 00:03:06 +0200

    scripts altered

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -45,17 +45,18 @@ export default function Layout({ children }) {
         <meta property="twitter:image" content="https://www.citybreak.pro/cover.png" />
 
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-6CCP477D8C"></script>
-        <script>{`window.dataLayer = window.dataLayer || [];
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-6CCP477D8C');`}
-        </script>
-        <script type="text/javascript">{`(function(c,l,a,r,i,t,y){
+gtag('config', 'G-6CCP477D8C');`}} />
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "kjuo8wbycs");`}</script>
+})(window, document, "clarity", "script", "kjuo8wbycs");`}} />
       </Head>
 
       <main>{children}</main>
