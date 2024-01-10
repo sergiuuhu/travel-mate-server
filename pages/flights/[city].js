4d7d1e1157b5a9e0b4eb1d560b298ab27420e2c9
Sergiu <sergiu.uhu@gmail.com>
2024-01-10 10:36:47 +0200

    disabled for now

--- pages/flights/[city].js
+++ pages/flights/[city].js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
 export default async function City() {
-  const router = useRouter()
+  // const router = useRouter()
 
-  const flights = await getData(router.query.city);
+  // const flights = await getData(router.query.city);
 
-  return <div>{flights.length} flights found.</div>;
+  // return <div>{flights.length} flights found.</div>;
+
+  return <></>
 }
 
-async function getData(city) {
-  const res = await fetch(`/api/fetch/${city}`, { next: { revalidate: 28800 } });
+// async function getData(city) {
+//   const res = await fetch(`/api/fetch/${city}`, { next: { revalidate: 28800 } });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
